263b111345ad7126ff445111fde2f6494475985e
noname9i <115953877+noname9i@users.noreply.github.com>
2024-07-06 20:28:52 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
 /*=============== SHOW MENU ===============*/
-
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close'),
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
+if(navToggle){
+  navToggle.addEventListener('click', () =>{
+    navMenu.classList.add('show-menu')
+  })
+}
 
 /*=============== SHADOW HEADER ===============*/
-
+if(navClose){
+  navClose.addEventListener('click', () =>{
+    navMenu.classList.remove('show-menu')
+  })
+}
 
 /*=============== SWIPER FAVORITES ===============*/ 
 
